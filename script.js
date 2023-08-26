e4ee69f6d2978c9e4640b8ebd13299e8c63ffcfa
Vineetg2003 <vineetgoyal130@gmail.com>
2023-08-26 22:02:41 +0530

    commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav_link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+const bgHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the background-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', bgHeader)
+
+
+
+gsap.from('.home__img2',1.2, {opacity:0, y:200 , delay : .1})
+gsap.from('.home__img3',1.2, {opacity:0, y:200 , delay : .5})
+gsap.from('.home__data',1.2, {opacity:0, y:-60 , delay : 1})
+gsap.from('.home__bird1',1.2, {opacity:0, x:-80 , delay : 1.1})
+gsap.from('.home__bird2',1.2, {opacity:0, x:80 , delay : 1.2})
+gsap.from('.home__img1',1.2, {opacity:0, y:200 , delay : 1.2})
+gsap.from('.home__img4',1.2, {opacity:0, x:200 , delay : 1.3})
